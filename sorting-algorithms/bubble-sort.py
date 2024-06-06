c053047d897e12355c835f377fcd1bc1157857a4
Lucas José <90628627+yamatosz@users.noreply.github.com>
2024-06-06 09:22:27 -0300

    bubble sort edit

--- sorting-algorithms/bubble-sort.py
+++ sorting-algorithms/bubble-sort.py
@@ -3,9 +3,13 @@ def bubble_sort(array):
     comparisons = 0
     swaps = 0
     for i in range(n):
+        swapped = False
         for j in range(n-i-1):
             comparisons+=1
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
                 swaps+=1
+                swapped = True
+        if not swapped:
+            break
     return array, comparisons, swaps
